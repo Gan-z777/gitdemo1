830523392d1cd5a14a83b397f55a5457a952852d
Ganz <787896309@qq.com>
2022-07-24 00:23:36 +0800

    第一次提交

---
+++ src/main/java/kgc/cn/demo.java
@@ -0,0 +1,12 @@
+package kgc.cn;
+
+/**
+ * @Author: GZ.
+ * @Description:
+ * @Date 2022-07-24 0:20
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello 123");
+    }
+}
